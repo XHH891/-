027ed6512cae5e92e300a88ea2a7038bb6035598
xhh <3397546390@qq.com>
2026-03-18 22:03:19 +0800

    Delete 目标检测/DETR directory

--- 目标检测/DETR/cs.py
+++
@@ -1,7 +0,0 @@
-import torch
-
-
-print("PyTorch 版本：", torch.__version__)
-print("CUDA 是否可用：", torch.cuda.is_available())
-print("CUDA 版本：", torch.version.cuda)  # 需安装CUDA版本的PyTorch
-print("当前使用的GPU：", torch.cuda.get_device_name(0) if torch.cuda.is_available() else "无GPU")
